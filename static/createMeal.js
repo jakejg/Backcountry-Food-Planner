9b99581c400028d3fc830855279f5cba38365f27
Jake <jakefrisbee@gmail.com>
2020-04-17 08:19:52 -0600

    cleaned up javascript

--- static/createMeal.js
+++ static/createMeal.js
@@ -1,21 +1,21 @@
-$mealForm = $('#send-ids')
-$searchList = $('#search-list')
-$mealList = $('#meal-list')
+$mealForm = $('#send-ids');
+$searchList = $('#search-list');
+$mealList = $('#meal-list');
 
-SELECTED_FOODS = []
+SELECTED_FOODS = [];
 
 BASE_URL = `https://${location.host}/meal/api`
 
 // clear send-ids form values
 for (let i=0; i<4; i++){
     
-    let input = $mealForm.children()
+    let input = $mealForm.children();
 
-    $(input[i]).val('')
+    $(input[i]).val('');
 }
 
 async function handlesubmit(evt){
-    evt.preventDefault()
+    evt.preventDefault();
 
     params = {
         "item": $('#food').val()
@@ -27,8 +27,7 @@ async function handlesubmit(evt){
 
     try{
         let resp = await axios.post(BASE_URL, {params})
-        console.log(resp)
-        displayResults(resp.data)
+        displayResults(resp.data);
     }
     catch(error){
        
@@ -37,19 +36,19 @@ async function handlesubmit(evt){
             let btalert = $(`<div class="alert alert-warning" role="alert">
             ${error.response.data.error}
           </div>`)
-          $('#top').prepend(btalert)
+          $('#top').prepend(btalert);
         }
     }
 }
 
 function displayResults(resp){
-    $searchList.empty()
+    $searchList.empty();
     for (food of resp){
         let $item = $(`<li class="list-group-item" data-id=${food.fdcId}>${food.description}<br>
             <small class="text-muted" data-id=${food.fdcId}>${food.ingredients}</small><br>
             ${food.brandOwner}</li>
             `)
-        $searchList.append($item)
+        $searchList.append($item);
         }
 }
 
@@ -57,56 +56,54 @@ function selectFood(evt){
     
     if (SELECTED_FOODS.length < 4){
         if (evt.target.tagName === 'LI') {
-            $item = $(evt.target)
+            $item = $(evt.target);
         }
         else{
-            $item = $(evt.target).parent()
+            $item = $(evt.target).parent();
         }
 
         //add color and spacing
-        $item.addClass('my-1 rounded').css('background-color', 'rgb(245, 235, 181)')
+        $item.addClass('my-1 rounded').css('background-color', 'rgb(245, 235, 181)');
         //add to my ingredients
-        $item.append('<i class="fas fa-trash m-1"></i>')
+        $item.append('<i class="fas fa-trash m-1"></i>');
        
 
-        $mealList.append($item)
+        $mealList.append($item);
         //add id to list
-        SELECTED_FOODS.push($item.attr('data-id')) 
+        SELECTED_FOODS.push($item.attr('data-id'));
     }
-
-    console.log(SELECTED_FOODS)
 }
 
 function deleteFood(evt){
-    $item = $(evt.target).parent()
+    $item = $(evt.target).parent();
  
     //remove from selected_foods array
     i = SELECTED_FOODS.findIndex(val => {
-        return val === $item.attr('data-id')
+        return val === $item.attr('data-id');
     })
 
-    SELECTED_FOODS.splice(i,1)
+    SELECTED_FOODS.splice(i,1);
     
-    $item.remove()
+    $item.remove();
 }
 
 function addFoodIds(evt){
    
     // stop form from submitting for now
-    evt.preventDefault()
+    evt.preventDefault();
 
     //add values to form inputs
     for (let i=0; i<SELECTED_FOODS.length; i++){
 
-        let input = $mealForm.children()
+        let input = $mealForm.children();
 
-        $(input[i]).val(SELECTED_FOODS[i])
+        $(input[i]).val(SELECTED_FOODS[i]);
     }
 
-    $mealForm[0].submit()
+    $mealForm[0].submit();
 }
 
-$mealForm.on('submit', addFoodIds)
-$('#search').on('submit', handlesubmit)
-$searchList.on('click', 'li', selectFood)
-$mealList.on('click', 'i', deleteFood)
+$mealForm.on('submit', addFoodIds);
+$('#search').on('submit', handlesubmit);
+$searchList.on('click', 'li', selectFood);
+$mealList.on('click', 'i', deleteFood);
