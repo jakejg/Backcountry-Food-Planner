b7f0cd52571e5452023560cc8f76c4c5d86414f7
Jake <jakefrisbee@gmail.com>
2020-04-16 18:43:35 -0600

    heroku files

--- app.py
+++ app.py
@@ -8,14 +8,13 @@ from sqlalchemy.exc import IntegrityError
 from werkzeug.exceptions import Unauthorized
 from datetime import datetime
 
-
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = (os.environ.get('DATABASE_URL', 'postgres:///food_planner'))
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 connect_db(app)
 
